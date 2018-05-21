fb5a544a9db24676a904d9ee9df1edc96817b24e
Tim Givois <tim.givois.mendez@gmail.com>
2018-05-21 18:06:15 -0500

    Fix standard

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ require('./app/routes')
 const server = app.listen(app.get('port'), function () {
   const host = server.address().address
   const port = server.address().port
-  console.log("=> NewsOS starting on http://%s:%s", host, port)
+  console.log('=> NewsOS starting on http://%s:%s', host, port)
   console.log('=> Ctrl-C to shutdown server')
 })
